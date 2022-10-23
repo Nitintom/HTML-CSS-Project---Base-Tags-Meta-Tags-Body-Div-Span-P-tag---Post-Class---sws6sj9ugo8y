222b94be29c3f0322e3ce24c8399d470d10db4d8
Nitintom <114360708+Nitintom@users.noreply.github.com>
2022-10-24 00:26:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
-
+function changeHomeImg() {
+    document.getElementById("mainImage").src =
+    "https://wallpaperaccess.com/full/1700222.jpg";
+  }
+  
+  function changeWorkImg() {
+    document.getElementById("mainImage").src =
+      "https://wallpaperaccess.com/full/268341.jpg";
+  }
+  function changeBlogImg() {
+    document.getElementById("mainImage").src =
+      "https://wallpaperaccess.com/full/2433830.jpg";
+  }
+  
+  function changeAboutUsImg() {
+    document.getElementById("mainImage").src =
+      "https://st.depositphotos.com/1038076/4870/i/450/depositphotos_48701865-stock-photo-about-us.jpg";
+  }
